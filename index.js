1ae480d25bc3d20c869a1da35c1a00b8ad646a76
Danniel Rolfe <talkto@dannielrolfe.com>
2021-11-06 12:14:10 -0700

    Add todo endpoint

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const hostname = '127.0.0.1';
 const port = 30000;
 
 const server = http.createServer((request, response) => {
+   let responsText = 'Hello World';
+
    response.statusCode = 200;
-   response.setHeader('Content-Type', 'text/plain');
-   response.end('Hello Kitty')
+   response.setHeader('Content-Type', 'application/json');
+
+     if (request.url === '/todo' && request.method === 'GET') {
+        responsText = 'Todo';
+    }
+
+   response.write(responsText);
+   response.end()
 });
 
 server.listen(port, hostname, () => {
